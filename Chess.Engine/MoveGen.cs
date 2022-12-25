fecd4cce54a3e12a0645d16a7a5cf8f8cc88404a
Khashayar N <Khashayar.n@outlook.com>
2022-12-25 14:46:48 +0100

    Add Class MoveGen

---
+++ Chess.Engine/MoveGen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.EngineCore
+{
+    public class MoveGen
+    {
+        private readonly Engine _engine;
+        private readonly Boards _boards;
+        
+
+        public MoveGen(Engine engine, Boards boards)
+        {
+            _engine = engine;
+            _boards = boards;
+            
+        }
+
+
+        public List<int> GenAllMoves()
+        {
+            List<Piece> position = new List<Piece>();
+            
+
+
+            List<int> move = new List<int>();
+            return move;
+        }
+    }
+}
